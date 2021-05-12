26f262d02e351c310234636f8cbf2aaec74b0062
LucasCapri <capriatalucas@gmail.com>
2021-05-12 17:57:00 -0300

    Tests update

---
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -0,0 +1,86 @@
+package dds.monedero.model;
+
+import dds.monedero.exceptions.MaximaCantidadDepositosException;
+import dds.monedero.exceptions.MaximoExtraccionDiarioException;
+import dds.monedero.exceptions.MontoNegativoException;
+import dds.monedero.exceptions.SaldoMenorException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cuenta {
+
+  private double saldo = 0; // TODO repetido en constructor
+  private List<Movimiento> movimientos = new ArrayList<>(); // TODO inicializado en constrct?
+
+  public Cuenta() {
+    saldo = 0;
+  }
+
+  public Cuenta(double montoInicial) {
+    saldo = montoInicial;
+  }
+
+  // CODE SMELL: Es ilogico querer agregar movimientos a una cuenta saltandose la modificacion del saldo de la misma
+  public void setMovimientos(List<Movimiento> movimientos) {
+    this.movimientos = movimientos;
+  }
+
+  public void poner(double cuanto) {
+    if (cuanto <= 0) {
+      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
+    }
+
+    // CODE SMELL, estamos duplicando el codigo que podemos delegar
+    // Fallo de logica, en ningun momento se chequea que sea en la misma fecha
+    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+      throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
+    }
+    // CODE SMELL, misplaced method agregateA
+    new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
+  }
+
+  public void sacar(double cuanto) {
+    if (cuanto <= 0) {
+      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
+    }
+    if (getSaldo() - cuanto < 0) {
+      throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
+    }
+    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
+    double limite = 1000 - montoExtraidoHoy;
+    if (cuanto > limite) {
+      throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
+          + " diarios, límite: " + limite);
+    }
+    new Movimiento(LocalDate.now(), cuanto, false).agregateA(this); // TODO cambiar por agregarMovimiento().
+  }
+
+  public void agregarMovimiento(LocalDate fecha, double cuanto, boolean esDeposito) {
+    Movimiento movimiento = new Movimiento(fecha, cuanto, esDeposito);
+    movimientos.add(movimiento);
+  }
+
+  public double getMontoExtraidoA(LocalDate fecha) {
+    return getMovimientos().stream()
+        // CODE SMELL: Duplicated code, missing delegation al objeto de tipo Movimiento que ya tiene el metodo fueExtraido(fecha)
+        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
+        .mapToDouble(Movimiento::getMonto)
+        .sum();
+  }
+
+  public List<Movimiento> getMovimientos() {
+    return movimientos;
+  }
+
+  public double getSaldo() {
+    return saldo;
+  }
+
+  // CODE SMELL estamos rompiendo el encapsulamiento de saldo innecesariamente.
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
+
+}
---
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -0,0 +1,101 @@
+package dds.monedero.model;
+
+import dds.monedero.exceptions.MaximaCantidadDepositosException;
+import dds.monedero.exceptions.MaximoExtraccionDiarioException;
+import dds.monedero.exceptions.MontoNegativoException;
+import dds.monedero.exceptions.SaldoMenorException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MonederoTest {
+  private Cuenta cuenta;
+
+
+  @BeforeEach
+  void init() {
+    cuenta = new Cuenta();
+  }
+
+  @Test
+  void sePuedeInstanciarCuentaConSaldoInicial(){
+    Cuenta cuentaConSaldoInicial = new Cuenta(40);
+    assertEquals(40,cuentaConSaldoInicial.getSaldo());
+  }
+  @Test
+  void SeLePuedePonerDeFormaSatisfactoria() {
+    cuenta.poner(1500);
+    assertEquals(1500,cuenta.getSaldo());
+  }
+
+  @Test
+  void ThrowExceptionCuandoTratoPonerMontoNegativo() {
+    assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
+  }
+
+  @Test
+  void PuedoHacerTresDepositosCorrectamente() {
+    cuenta.poner(1500);
+    cuenta.poner(456);
+    cuenta.poner(1900);
+    assertEquals(3856,cuenta.getSaldo());
+  }
+
+  @Test
+  void NoPuedoHacerMasDeTresDepositosEnUnDia() {
+    assertThrows(MaximaCantidadDepositosException.class, () -> {
+          cuenta.poner(1500);
+          cuenta.poner(456);
+          cuenta.poner(1900);
+          cuenta.poner(245);
+
+    });
+  }
+  @Test
+  void PuedoHacerMasDeTresDepositosSiNoSonElMismoDia(){
+    // Agrego a mano para simular la fecha
+    assertDoesNotThrow(()-> {
+      cuenta.agregarMovimiento(LocalDate.of(2000,2,2),100,true);
+      cuenta.poner(1500);
+      cuenta.poner(456);
+      cuenta.poner(1900);
+    });
+
+  }
+
+  @Test
+  void ExtraerMasQueElSaldo() {
+    assertThrows(SaldoMenorException.class, () -> {
+          cuenta.setSaldo(90); // TODO rompo encapsulamiento
+          cuenta.sacar(1001);
+    });
+  }
+
+  @Test
+  public void ExtraerMasDe1000() {
+    assertThrows(MaximoExtraccionDiarioException.class, () -> {
+      cuenta.setSaldo(5000); // TODO rompo encapsulamiento
+      cuenta.sacar(1001);
+    });
+  }
+
+  @Test
+  public void ExtraerMontoNegativo() {
+    assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
+  }
+
+  @Test
+  public void SeSiEsDepositoYFecha(){
+    Movimiento movimiento = new Movimiento(LocalDate.now(),100,true);
+    assertEquals(true,movimiento.fueDepositado(LocalDate.now()));
+  }
+  @Test
+  public void SeSiEsExtraccionYFecha(){
+    Movimiento movimiento = new Movimiento(LocalDate.now(),100,false);
+    assertEquals(true,movimiento.fueExtraido(LocalDate.now()));
+  }
+
+}
